792c8e762ffe6e6ee064e1e0b0fa2fe9e29165b6
Lilin Xu <66715872+lilinxu-caicloud@users.noreply.github.com>
2020-06-12 16:54:19 +0800

    update

--- returnIP.go
+++ returnIP.go
@@ -7,11 +7,18 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	a := strings.Split(r.RemoteAddr, ":")
+
 	//fmt.Printf("%s",  a[0])
-	fmt.Fprintf(w,"%s",  a[0])
+	_,err:=fmt.Fprintf(w,"%s",  a[0])
+	if err!=nil{
+		fmt.Println("Error:",err)
+	}
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/myip", handler)
+	err:=http.ListenAndServe(":8080", nil)
+	if err!=nil{
+		fmt.Println("Error:",err)
+	}
 }
